97cbeb6a135775a6b418109d75f25f7192169bd4
Anna Bondarenko <anna93bondarenko@gmail.com>
2021-06-11 00:38:18 +0300

    second task done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+
+const ingredientsContainerEl = document.querySelector('#ingredients')
+
+const ingredientsEl = ingredients.map(ingredient => {
+  const ingredientEl = document.createElement('li');
+  ingredientEl.textContent = ingredient;
+  return ingredientEl;
+})
+console.log(ingredientsEl)
+
+ingredientsContainerEl.append(...ingredientsEl)
+console.log(ingredientsContainerEl)
