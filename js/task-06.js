678e208d585c2bb5776e58aefe9cb9121e749f13
Anna Bondarenko <anna93bondarenko@gmail.com>
2021-06-12 19:35:57 +0300

    add task 6

---
+++ js/task-06.js
@@ -0,0 +1,16 @@
+
+const inputEl = document.querySelector('#validation-input');
+const dataAttributeValue = inputEl.dataset.length;
+
+inputEl.addEventListener('blur', onInputElBlur);
+
+function onInputElBlur() {
+    if (inputEl.value.length === Number(dataAttributeValue)) {
+        inputEl.classList.add('valid');
+        inputEl.classList.remove('invalid');
+    }
+    if (inputEl.value.length !== Number(dataAttributeValue)) {
+        inputEl.classList.add('invalid');
+        inputEl.classList.remove('valid');
+    }
+};
