e37ffd17f0af1f1bbfe197d56f2d9db7ed202d8a
Anna Bondarenko <anna93bondarenko@gmail.com>
2021-06-22 20:46:31 +0300

    вносит правки

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,7 @@
 
 const sizeControl = document.querySelector('#font-size-control')
 const textEl = document.querySelector('#text')
+textEl.style.fontSize = `${sizeControl.value}px`
 
 sizeControl.addEventListener('input', onSizeControlInput)
 function onSizeControlInput() {
